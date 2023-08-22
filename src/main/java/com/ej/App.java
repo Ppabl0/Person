9dcd9a48ec045a503d95c646b5eea52cbb511149
Ppabl0 <alvarado.josepablo.mx@gmail.com>
2023-08-22 09:25:52 -0600

    ADD sample for jenkins

---
+++ src/main/java/com/ej/App.java
@@ -0,0 +1,16 @@
+package com.ej;
+public class App {
+    public static void main( String[] args ) {
+        System.out.println( "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" );
+        System.out.println("           Jenkins docker pipeline sonarqub     ");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    }
+
+    public int noSum(int sumand){
+        return 30 + sumand;
+    }
+
+    public boolean noTestead(boolean exp){
+        return exp;
+    }
+}
---
+++ src/test/java/com/ej/AppTest.java
@@ -0,0 +1,18 @@
+package com.ej;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        App x = new App();
+        assertEquals(29, x.noSum(-1));
+        assertTrue( true );
+    }
+}
