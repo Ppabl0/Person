81222d3d08ee51ab7283a15010ffbbdae9f6f827
Ppabl0 <alvarado.josepablo.mx@gmail.com>
2023-08-25 13:38:25 -0600

    PASS make quality gate pass for demo

--- src/main/java/com/ej/DemoRestController.java
+++ src/main/java/com/ej/DemoRestController.java
@@ -33,9 +33,9 @@ public class DemoRestController {
 
     public static int suma5(){ return 5;}
 
-//    public static int suma6(){return 0b110;}
-//
-//    public static int suma7(){return 0x7;}
+    public static int suma6(){return 0b110;}
+
+    public static int suma7(){return 0x7;}
 
 //    private static int shuuu(int aaaaaa, String xxxx){
 //        //aaaaaaaaaaaaaaaaaaaaaaaaaaa
