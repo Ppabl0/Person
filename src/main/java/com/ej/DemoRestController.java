59c8caf8f8be1dc55e05f9a1b9633f556fe76281
Ppabl0 <alvarado.josepablo.mx@gmail.com>
2023-08-25 11:40:48 -0600

    PASS try githubwebkuk

--- src/main/java/com/ej/DemoRestController.java
+++ src/main/java/com/ej/DemoRestController.java
@@ -33,10 +33,10 @@ public class DemoRestController {
 
     public static int suma5(){ return 5;}
 
-//    public static int suma6(){return 0b110;}
-//
-//    public static int suma7(){return 0x7;}
-//
+    public static int suma6(){return 0b110;}
+
+    public static int suma7(){return 0x7;}
+
 //    private static int shuuu(int aaaaaa, String xxxx){
 //        //aaaaaaaaaaaaaaaaaaaaaaaaaaa
 //        //bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
